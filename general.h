10a192c1a9a522c42be38ffa6e5e500834a287d2
charlie-bennett <35985336+charlie-bennett@users.noreply.github.com>
2020-10-19 00:36:00 -0700

    Update general.h

--- general.h
+++ general.h
@@ -55,16 +55,17 @@ int world_size;
 #define MAX_NUM_SIZE 10
 #define MAX_WORD_SIZE 50 
 #define INPUT_FILE_PATH "2600.txt"
+#define OUTPUT_FILE_PATH "2600.out"
 #define PRINT_EXCHANGE_NUMS 1
 #define PRINT_BATMEN_REDUCER_MAPS 0
 #define PRINT_MASTER_MAP 0
-#define PRINT_MASTER_MAP_PERIOD 100
+#define PRINT_MASTER_MAP_PERIOD 1
 #define MODE_WORD_CHAR 0
 #define HEAP_SIZE 10
-#define PRINT_HEAP 1
+#define PRINT_HEAP 0
 #define PRINT_HEAP_PERIOD 10
 
-//we could combine these for efficiency 
+// Sending data management 
 const int INTRA_BUFF_SIZE = INTRANODE_BLOCK_SIZE*(MAX_WORD_SIZE + MAX_NUM_SIZE); 
 const int INTER_BUFF_SIZE = INTERNODE_BLOCK_SIZE*(MAX_WORD_SIZE + MAX_NUM_SIZE); 
 char intraNodeOutbox[INTRANODE_OUTBOX_SIZE][INTRA_BUFF_SIZE];
@@ -76,28 +77,11 @@ int master_batman;
 int master_robin; 
 MPI_Request* intraNodeOutboxRequests[INTRANODE_OUTBOX_SIZE]; 
 MPI_Request* interNodeOutboxRequests[INTERNODE_OUTBOX_SIZE]; 
-
-
-
 char intraNodeBuffer[INTRA_BUFF_SIZE];
 char interNodeBuffer[INTRA_BUFF_SIZE];
 int intraNodeBufferSize;
 int interNodeBufferSize; 
 int current_file_index; 
-
-
-void get_words(map<string, int>& output_map, ifstream& fl, int& current_index, int start_index,  int stop_index);
-void read_in_block_intra(ifstream& fl, int start_index, int stop_index);
-void read_in_block_inter(ifstream& fl, int start_index, int stop_index);
-void flatten_map(char* output, int& current_index, map<string, int>& input_map, int stop_index);
-static void prep_word(string& cword);
-static void number_as_chars(int num, char *dest, int& output_len); 
-void unflatten_map(char* input_chars, int& _current_index, map<string, int>& input_map, int stop_index);
-void unflatten_map_maintain_heap(char* input_chars, int& _current_index, map<string, int>& input_map, int stop_index); 
-void get_chars(map<string, int>& output_map, ifstream& fl, int& _current_index, int start_index,  int stop_index);
-void get_most_least_frequent(); 
-
-
 
 struct reduceArgs
 {
@@ -105,29 +89,53 @@ struct reduceArgs
     int start_read_loc; 
     int stop_read_loc;
     int my_partner;
-    
     ifstream* input_file; 
-    int is_end; 
 };
 
+//Core Logic
+void do_reduce(reduceArgs* input_args); 
+void do_reduce_sidekick(reduceArgs* input_args); 
+void do_master(); 
+void do_master_sidekick();
+
+//Core Text Processing 
+void read_in_block_intra(ifstream& fl, int start_index, int stop_index);
+void read_in_block_inter(ifstream& fl, int start_index, int stop_index);
+void flatten_map(char* output, int& current_index, map<string, int>& input_map, int stop_index);
+void unflatten_map(char* input_chars, int& _current_index, map<string, int>& input_map, int stop_index);
+void get_words(map<string, int>& output_map, ifstream& fl, int& current_index, int start_index,  int stop_index);
+void get_chars(map<string, int>& output_map, ifstream& fl, int& _current_index, int start_index,  int stop_index);
+
+//Helper Funcitons
+template <typename PQtype>
+void print_pq(PQtype input_pq); 
+void print_map_to_file(std::map<std::string, int> map, char* filename); 
+
+static void prep_word(string& cword);
+bool is_valid_char(char num);
+static void number_as_chars(int num, char *dest, int& output_len); 
+void get_most_least_frequent(); 
+
+map<string, int> working_map; 
+
+
+
+
+//Maintaining List of 10 most and 10 least frequent observations 
+
 struct min_comapare_obs_freq{
-    bool operator()(const std::pair<string, int>& first, const std::pair<string, int>& second) const { return first.second < first.second;}
+    bool operator()(const std::pair<string, int>& one, const std::pair<string, int>& two) const { return one.second > two.second;}
 };
 
 struct max_comapare_obs_freq{
-    bool operator()(const std::pair<string, int>& first, const std::pair<string, int>& second) const { return first.second < first.second;}
+    bool operator()(const std::pair<string, int>& one, const std::pair<string, int>& two) const { return one.second < two.second;}
 };
 
-
-map<string, int> working_map; 
-//priority_queue<std::pair<string, int>, min_comapere_obs_freq> min_heap; 
 priority_queue<std::pair<string, int>, std::vector<std::pair<string, int>>, min_comapare_obs_freq>  min_heap; //Most frequent 
 priority_queue<std::pair<string, int>, std::vector<std::pair<string, int>>, max_comapare_obs_freq> max_heap; //Least frequent
 std::set<int, std::greater<int>> largest_frequencies; 
 std::set<int, std::less<int>> smallest_frequencies;  
 
-void do_reduce(reduceArgs* input_args); 
-void do_reduce_sidekick(reduceArgs* input_args); 
-void do_master(); 
-void do_master_sidekick();
+
+
 
